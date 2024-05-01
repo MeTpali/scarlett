602f45395abccc7ed077fa1620f2890fcd533626
MeTpali <tereshchukartemy2003@mail.ru>
2024-05-01 19:58:11 +0300

    delete trash

--- dota_web_scrapper/android/app/src/main/kotlin/com/example/dota_web_scrapper/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dota_web_scrapper
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- socket_chat/android/app/src/main/kotlin/com/example/socket_chat/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.socket_chat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
