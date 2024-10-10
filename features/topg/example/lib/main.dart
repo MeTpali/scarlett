dc4cec3627c7334ee79e752b3967f5ba67687d6b
MeTpali <tereshchukartemy2003@mail.ru>
2024-10-10 09:32:25 +0300

    intermediate commit

--- features/topg/example/lib/main.dart
+++ features/topg/example/lib/main.dart
@@ -1,36 +1,91 @@
+import 'package:example/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:i18n/i18n.dart';
 import 'package:topg/topg.dart';
 
-import 'src/routes/routes.dart';
-
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ScarlettLocalization.init();
   await TopG.init();
-  runApp(const ExampleApp());
+  runApp(
+    const TopG(
+      child: MyApp(),
+    ),
+  );
 }
 
-class ExampleApp extends StatefulWidget {
-  const ExampleApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<ExampleApp> createState() => _ExampleAppState();
-}
-
-class _ExampleAppState extends State<ExampleApp> {
-  final _router = ExampleRouter();
-  @override
-  Widget build(BuildContext context) => TopG(
-        child: MaterialApp.router(
-          title: 'TopG example',
-          theme: ThemeData(
-            useMaterial3: true,
-          ),
-          routerConfig: _router.config(),
+  Widget build(BuildContext context) {
+    return ScarlettLocalization(builder: (locale) {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        locale: locale,
+        supportedLocales: S.supportedLocales,
+        localizationsDelegates: S.localizationDelegates,
+        theme: ThemeData(
+          colorScheme: TopGColorScheme.custom,
+          useMaterial3: true,
         ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       );
+    });
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
   @override
-  void dispose() {
-    super.dispose();
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          SettingsButton(onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const SettingsScreen(),
+              ),
+            );
+          })
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
